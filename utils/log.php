fda49a741b032d01d3c3b2c386271cb51a7f18a2
Waldek <sp2ong@wp.pl>
2019-06-26 20:25:22 +0200

    Update log.php

--- utils/log.php
+++ utils/log.php
@@ -29,7 +29,7 @@ echo "a:link { text-decoration: none; }";
 echo "</style>";
 echo "<meta http-equiv=\"refresh\" content=\"30\"></head>\n\n";
 echo "<body><font face=\"arial,helvetica\"><font size=\"+1\">\n\n";
-echo "<H2 align=\"center\">Activity & Last Heard Dashboard HBlink3 Białystok</H2>";
+echo "<H2 align=\"center\">Activity & Last Heard Dashboard HBlink3</H2>";
 echo "<p align=\"middle\">\n";
 echo "<font size=\"-2\">&copy; developed by DL1BZ as logging-extension of <A HREF=\"https://github.com/n0mjs710/HBmonitor\">HBmonitor</A> (2018,2019)</font><BR>\n";
 echo "<div style=\"overflow-x:auto;\">\n\n";
