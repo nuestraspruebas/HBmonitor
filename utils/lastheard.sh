7928344f0e68700e1340520d8201562fe8a6b926
Waldek <sp2ong@wp.pl>
2019-06-26 21:01:09 +0200

    Update lastheard.sh

--- utils/lastheard.sh
+++ utils/lastheard.sh
@@ -3,3 +3,4 @@ mv /var/www/html/lastheard.log /var/www/html/lastheard.log.save
 /usr/bin/tail -550 /var/www/html/lastheard.log.save > /var/www/html/lastheard.log
 mv /var/www/html/lastheard.log /var/www/html/lastheard.log.save
 /usr/bin/tail -550 /var/www/html/lastheard.log.save > /var/www/html/lastheard.log
+
