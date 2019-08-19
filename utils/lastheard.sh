620e52559c94dc56f0374f2805c29c90a8d0ca8a
Waldek <sp2ong@wp.pl>
2019-08-19 12:51:15 +0200

    Delete lastheard.sh

--- utils/lastheard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mv /var/www/html/lastheard.log /var/www/html/lastheard.log.save
-/usr/bin/tail -550 /var/www/html/lastheard.log.save > /var/www/html/lastheard.log
-mv /var/www/html/lastheard.log /var/www/html/lastheard.log.save
-/usr/bin/tail -550 /var/www/html/lastheard.log.save > /var/www/html/lastheard.log
-
