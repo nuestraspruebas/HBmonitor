c4794e5c5e8c0cc6cb03c06e4991789c7ff69dc0
Waldek <sp2ong@wp.pl>
2019-09-17 21:16:48 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# Install the required support programs
-apt-get install python python-pip python-dev libffi-dev libssl-dev -y
-pip2 install setuptools wheel
-pip2 install -r requirements.txt
