eed1671668c987e647727e25d685387da5436c5e
Waldek <sp2ong@gmail.com>
2020-07-21 09:44:33 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -638,15 +638,15 @@ def process_message(_bmessage):
             elif p[1] == 'START':
                 log_message = '{} {} {} SYS: {:8.8s} SRC: {:9.9s}; {:9.9s} TS: {} TGID: {:7.7s} {:17.17s} SUB: {:9.9s}; {:18.18s}'.format(_now[10:19], p[0][6:], p[1], p[3], p[5], alias_call(int(p[5]), subscriber_ids), p[7],p[8], alias_tgid(int(p[8]),talkgroup_ids), p[6], alias_short(int(p[6]), subscriber_ids))
             elif p[1] == 'END WITHOUT MATCHING START':
-                log_message = '{}: {} {} on SYSTEM {:8.8s}: SRC: {:9.9s}; {:9.9s} TS: {} TGID: {:7.7s} {:17.17s} SUB: {:9.9s}; {:18.18s}'.format(_now[10:19], p[0][6:], p[1], p[3], p[5], alias_call(int(p[5]), subscriber_ids), p[7], p[8],alias_tgid(int(p[8]),talkgroup_ids),p[6], alias_short(int(p[6]), subscriber_ids))
+                log_message = '{} {} {} on SYSTEM {:8.8s}: SRC: {:9.9s}; {:9.9s} TS: {} TGID: {:7.7s} {:17.17s} SUB: {:9.9s}; {:18.18s}'.format(_now[10:19], p[0][6:], p[1], p[3], p[5], alias_call(int(p[5]), subscriber_ids), p[7], p[8],alias_tgid(int(p[8]),talkgroup_ids),p[6], alias_short(int(p[6]), subscriber_ids))
             else:
-                log_message = '{} UNKNOWN GROUP VOICE LOG MESSAGE'.format(_now)
+                log_message = '{} UNKNOWN GROUP VOICE LOG MESSAGE'.format(_now[10:19])
 
             dashboard_server.broadcast('l' + log_message)
             LOGBUF.append(log_message)
 
         else:
-            logging.debug('{}: UNKNOWN LOG MESSAGE'.format(_now))
+            logging.debug('{} UNKNOWN LOG MESSAGE'.format(_now[10:19]))
 
     else:
         logging.debug('got unknown opcode: {}, message: {}'.format(repr(opcode), repr(_message[1:])))
